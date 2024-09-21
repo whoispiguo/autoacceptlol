f4f71376df05f2449f4ab04e5ed63d4b87579981
whoispiguo <115208809+whoispiguo@users.noreply.github.com>
2024-09-20 23:42:25 -0300

    v2.0
    added autopick champ

--- autoaccept.py
+++ autoaccept.py
@@ -12,7 +12,7 @@ def find_image_in_screenshot(image_path, screenshot_path):
     screenshot = cv2.imread(screenshot_path, cv2.IMREAD_GRAYSCALE)
     
     if template is None or screenshot is None:
-        raise FileNotFoundError("No se pudo cargar la imagen. Verifica que los archivos estén en la misma carpeta que el script.")
+        raise FileNotFoundError("bug, report at github/whoispiguo pls!")
     
     result = cv2.matchTemplate(screenshot, template, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
@@ -21,21 +21,35 @@ def find_image_in_screenshot(image_path, screenshot_path):
         return max_loc
     return None
 
-def click_on_image(image_path):
+def click_on_image(image_path, click_location, text_to_type=None):
     screenshot_path = take_screenshot()
     location = find_image_in_screenshot(image_path, screenshot_path)
     
     if location:
-        pyautogui.click(location[0] + 360, location[1] + 600)
-        print("Partida aceptada.")
-
+        pyautogui.click(click_location[0], click_location[1])
+        if text_to_type:
+            pyautogui.typewrite(text_to_type)
+        print(f"done.")
     else:
-        print("No salta cola.")
+        print(f"waiting queue.")
+
+def image_exists(image_path):
+    screenshot_path = take_screenshot()
+    location = find_image_in_screenshot(image_path, screenshot_path)
+    return location is not None
 
 def main():
+    text_to_type = input("Pick champ: ")
     while True:
-        click_on_image('found.png')
+        click_on_image('found.png', (360, 600))
         time.sleep(5)
+        if image_exists('search.png'):
+            click_on_image('search.png', (1220, 190), text_to_type)
+            time.sleep(1)
+            click_on_image('search.png', (620, 290))
+            time.sleep(3)
+            click_on_image('pick.png', (920,850))
+
 
 if __name__ == "__main__":
     main()
